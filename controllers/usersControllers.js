1a732a9d1104c0d960e2a16db74b9631c24018cf
edenlisk <edenlisk>
2023-07-31 17:21:09 +0200

    Add users controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,2 +1,72 @@
+const User = require('../models/usersModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const { logger } = require('../utils/loggers');
 
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+    res
+        .status(200)
+        .json(
+            {
+                status: "Success",
+                data: {
+                    users
+                }
+            }
+        )
+    ;
+})
 
+exports.getOneUser = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.params.userId);
+    if (!user) return next(new AppError("The Selected user no longer exists!", 400));
+    res
+        .status(200)
+        .json(
+            {
+                status: "Success",
+                data: {
+                    user
+                }
+            }
+        )
+    ;
+})
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.params.userId);
+    if (!user) return next(new AppError("The Selected user no longer exists!", 400));
+    if (req.body.permissions) user.permissions = req.body.permissions;
+    if (req.body.role) user.role = req.body.role;
+    if (req.body.active === true) {
+        user.active = true;
+        logger.info(`${user.name}'s account re-activated successfully`);
+    }
+    if (req.body.active === false) {
+        user.active = false;
+        logger.warn(`${user.name}'s account suspended/de-activated successfully`);
+    }
+    await user.save({validateModifiedOnly: true});
+    res
+        .status(202)
+        .json(
+            {
+                status: "Success"
+            }
+        )
+    ;
+})
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    logger.warn(`${user.name}'s account deleted successfully`);
+    res
+        .status(204)
+        .json(
+            {
+                status: "Success"
+            }
+        )
+    ;
+})
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const { getAllUsers, getOneUser, updateUser, deleteUser} = require('../controllers/usersControllers');
 const { signup, login, logout } = require('../controllers/authControllers');
+
+router.route('/')
+    .get(getAllUsers)
+
+router.route('/:userId')
+    .get(getOneUser)
+    .patch(updateUser)
+    .delete(deleteUser)
 
 router.route('/signup')
     .post(signup)
