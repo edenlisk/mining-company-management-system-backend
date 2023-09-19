d083a987811eed18e7ddbf44e97518dff86612fe
edenlisk <edenlisk>
2023-09-19 12:01:35 +0200

    Add get settings endpoint

--- controllers/settingsControllers.js
+++ controllers/settingsControllers.js
@@ -1,7 +1,21 @@
 const Settings = require('../models/settingsModel');
 const catchAsync = require('../utils/catchAsync');
 
-
+exports.getSettings = catchAsync(async (req, res, next) => {
+    const settings = await Settings.findOne();
+    if (!settings) return next(new AppError("Settings are currenly unavailable", 400));
+    res
+        .status(200)
+        .json(
+            {
+                status: "Success",
+                data: {
+                    settings
+                }
+            }
+        )
+    ;
+})
 
 exports.addSetting = catchAsync(async (req, res, next) => {
     await Settings.create(
--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const {addSetting, updateSettings} = require('../controllers/settingsControllers');
+const {addSetting, updateSettings, getSettings} = require('../controllers/settingsControllers');
 const router = Router();
 
 router.route('/')
+    .get(getSettings)
     .post(addSetting)
     .patch(updateSettings)
 
